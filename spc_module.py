2fad63f84674ef3d89c6e4311a74bbbae7a2b75d
angel <angel@aibdt.com.tw>
2023-03-09 11:46:56 +0800

    fix: add STATION_STEP filed

--- spc_module.py
+++ spc_module.py
@@ -5,14 +5,14 @@ fab_folder = "S3"
 data_source = "SPC"
 type_dict = {"Fab": str, "Station": str, "Department": str, "Filename": str, "Product": str, "Lot": str, "LAYER": str,
              "STEP": str, "Parameter": str, "PROPERTY": str, "EquipmentID": str, }
-data_type = {"FAB_ID": str, "PROD_ID_RAW": str, "LOT_ID": str, "TIME": object, "STEP": str,
+data_type = {"FAB_ID": str, "PROD_ID_RAW": str, "LOT_ID": str, "TIME": object, "STEP": str, "STATION_STEP": str,
              "PARAMETER_ID": str, "VALUE": int, "EQP_ID": str, "PROPERTY": str, "STATION": str,
              "DEPARTMENT": str, "FILE_NAME": str, "FILE_ID": int, "CTRL_ID": int, "PROD_ID": str, "LOT_TYPE": str}
 # After import data, the first step is to capitalize column names.
 column_name_format_list = ["FAB", "STATION", "DEPARTMENT", "FILENAME", "FILEID", "PRODUCT", "LOT", "LAYER", "STEP",
                            "PARAMETER", "CTRLID", "VALUE", "TIME", "PROPERTY", "DATATYPE", "EQUIPMENTID"]
 use_column_list = ["FAB_ID", "PROD_ID_RAW", "LOT_ID", "STEP", "PARAMETER_ID", "VALUE", "TIME", "EQP_ID", "PROPERTY",
-                   "STATION", "DEPARTMENT", "FILE_NAME", "FILE_ID", "CTRL_ID"]
+                   "STATION", "DEPARTMENT", "FILE_NAME", "FILE_ID", "CTRL_ID", "LAYER", "STATION_STEP"]
 replace_column_list = {"FAB": "FAB_ID", "PRODUCT": "PROD_ID_RAW", "LOT": "LOT_ID", "PARAMETER": "PARAMETER_ID",
                        "EQUIPMENTID": "EQP_ID", "FILENAME": "FILE_NAME", "FILEID": "FILE_ID", "CTRLID": "CTRL_ID"}
 
@@ -29,6 +29,7 @@ class SPCIdatamation(IdatamationFlow):
         df["TIME"] = df["TIME"].dt.tz_localize("Etc/GMT-8").dt.tz_convert("UTC")
         # TODO: fix to replace()"10XXXX" -> S3
         df["FAB"] = self.fab_folder
+        df["STATION_STEP"] = df["STEP"]
         df["STEP"] = df["LAYER"].astype(str) + "-" + df["STEP"].astype(str)
         df = df.rename(columns=replace_column_list)
         df = df[use_column_list]
